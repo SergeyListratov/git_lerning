d33b330b95a07b890f0cf7796ca6270020d99ebb
Sergey <sergey.listratov@gmail.com>
2023-02-14 17:11:47 +0300

    step4 - 2

--- Step_4/4.2.19.py
+++ Step_4/4.2.19.py
@@ -1,10 +1,5 @@
 import csv
-
-
-def read_csv_list(filename: str, deli=',') -> list:
-    with open(filename, 'r', encoding='utf-8') as file:
-        data_list = list(csv.DictReader(file, delimiter=deli))
-        return data_list
+from datetime import datetime
 
 
 def write_csv_list(filename: str, lst: list, col: list, deli=','):
@@ -14,13 +9,26 @@ def write_csv_list(filename: str, lst: list, col: list, deli=','):
         writer.writerows(lst)
 
 
-def email_div(st: str) -> str:
-    return st[(st.find('@') + 1):]
-
-
 if __name__ == '__main__':
-    columns, row_dict = ['domain', 'count'], dict()
-    for st in read_csv_list('name_log.csv'):
-        row_dict[email_div(st['email'])] = row_dict.get(email_div(st['email']), 0) + 1
-    new_list = [{'domain': t[0], 'count': t[1]} for t in sorted(row_dict.items())]
-    write_csv_list('domain_usage.csv', sorted(new_list, key=lambda i: i['count']), columns)
+    columns, row_dict = ['username', 'email', 'dtime'], dict()
+    with open('name_log.csv', 'r', encoding='utf-8') as file:
+        for st in csv.DictReader(file, delimiter=','):
+            if st['email'] in row_dict:
+                if datetime.strptime(st['dtime'], '%d/%m/%Y %H:%M') > datetime.strptime(row_dict[st['email']][1],
+                                                                                        '%d/%m/%Y %H:%M'):
+                    row_dict[st['email']] = (st['username'], st['dtime'])
+            else:
+                row_dict[st['email']] = (st['username'], st['dtime'])
+
+    nw_list = list(map(lambda i: {'username': i[1][0], 'email': i[0], 'dtime': i[1][1]}, sorted(row_dict.items())))
+    write_csv_list('new_name_log.csv', nw_list, columns)
+
+    with open('name_log.csv', encoding='UTF-8') as f:
+        header, *rows = csv.reader(f)
+
+    d = {i[1]: i for i in sorted(rows, key=lambda x: datetime.strptime(x[2], '%d/%m/%Y %H:%M'))}
+
+    with open('new_name_log.csv', 'w', encoding='UTF-8', newline='') as f:
+        w = csv.writer(f)
+        w.writerow(header)
+        w.writerows(sorted(d.values(), key=lambda x: x[1]))
